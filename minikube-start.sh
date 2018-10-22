ce3cb8dfff78ed28b961da1c910a991a553b4005
Staffan Olsson <staffan@repos.se>
2018-10-22 16:11:18 +0200

    Clever

--- minikube-start.sh
+++ minikube-start.sh
@@ -41,6 +41,7 @@ kubectl apply -f templates/registry-cert-job.yaml
 until kubectl certificate approve registry-tls 2>/dev/null;
 do
   echo "Waiting for registry cert job to request a certificate ..."
+  sleep 1
 done
 
 echo "Starting registry ..."
